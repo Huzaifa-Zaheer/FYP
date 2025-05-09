6509c3ee880472cabbaf0046e7787cc132424300
Huzaifa-Zaheer <mhuzaifazaheer@gmail.com>
2025-05-09 17:30:14 +0500

    Chat Integraton Updated

--- frontend/src/pages/Captainlogin.jsx
+++ frontend/src/pages/Captainlogin.jsx
@@ -38,7 +38,8 @@ const Captainlogin = () => {
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
-        <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+        {/* <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" /> */}
+        <h1 className='text-4xl font-bold mb-10 text-black-600'>HomeAssist</h1>
 
         <form onSubmit={(e) => {
           submitHandler(e)
